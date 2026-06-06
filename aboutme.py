94c2903a5396d1e4fa22d53177cf827fc2515caa
bemo-codes <shivamxapp@gmail.com>
2026-06-06 23:44:41 +0530

    Add files via upload
    Add about-me program - first commit

---
+++ aboutme.py
@@ -0,0 +1,6 @@
+name = input("What's you name?: ")
+age = input("Age?: ") 
+city = input("Which city do you live in?: ")
+fav_sub = input("Which is your fav subject?: ")
+
+print(f"{name} is {age} years old, lives in {city} and loves {fav_sub}.")
